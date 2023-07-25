eb85efa40ab625a3a104f4c31a0a980ce649126e
Galaxy <js_park95@naver.com>
2023-07-25 10:41:28 +0900

    브라우저 상단으로 스크롤하는 기능 추가

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,8 @@
-window.addEventListener('load', (event => {
+const scroller = new Scroller(false) // 스크롤 객체 생성
+
+
+
+window.addEventListener('load', (event) => {
     //테마변경 (다크모드/일반모드)
     const mode = document.querySelector('.mode')
     const header = document.querySelector('header')
@@ -14,4 +18,19 @@ window.addEventListener('load', (event => {
             icon.classList.add('active')
         }
     })
-}))
+
+    // 브라우저 상단으로 스크롤링 하기
+    const arrowUp = document.querySelector('.footer .icons .scroll-up')
+    arrowUp.addEventListener('click', (event) => {
+        history.pushState({}, "", '#') // URL 주소 초기화
+        scroller.setScrollPosition({top: 0, behavior: 'smooth'})
+    })
+
+    const logo = document.querySelector('header .logo')
+    logo.addEventListener('click', (event) => {
+        event.preventDefault() // a 태그의 기본적인 동작 제거
+        history.pushState({}, "", '#') // URL 주소 초기화
+        scroller.setScrollPosition({top: 0, behavior: 'smooth'})
+    })
+
+})
