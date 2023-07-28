79f7c2b7767505662d0c69762281807878309f62
Galaxy <js_park95@naver.com>
2023-07-28 14:12:11 +0900

    포스트 페이지 이미지 프리뷰 구현

--- js/post.js
+++ js/post.js
@@ -56,4 +56,74 @@ window.addEventListener('load', (event) => {
 
     })
 
+    // 파일 입력 처리
+    let lastCaretLine = null // Caret: 커서 (커서 위치의 엘리먼트 저장)
+    const uploadInput = document.querySelector('.upload input')
+    uploadInput.addEventListener('change', function(event){
+        const files = this.files
+        console.log(files)
+
+        if(files.length > 0){
+            for(const file of files){
+                const fileType = file.type
+
+                if(fileType.includes('image')){
+                    console.log('image')
+                    const img = document.createElement('img')
+                    img.src = URL.createObjectURL(file) // 파일 임시경로 생성
+                    // console.log(URL.createObjectURL(file))
+                    lastCaretLine = addFileToCurrentLine(lastCaretLine, img)
+                }else if(fileType.includes('video')){
+                    console.log('video')
+
+                }else if(fileType.includes('audio')){
+                    console.log('audio')
+
+                }else{
+                    console.log('file')
+
+                }
+            }
+
+            // 커서 위치를 맨 마지막으로 추가한 파일 아래쪽에 보여주기
+            // 사용자가 드래그로 선택한 범위
+            const selection = document.getSelection()
+            selection.removeAllRanges()
+
+            // 해당 엘리먼트를 범위로 지정
+            const range = document.createRange()
+            range.selectNodeContents(lastCaretLine)
+            range.collapse() // 범위가 아니라 커서 지정
+            selection.addRange(range) // 새로운 범위가 설정
+
+            postContents.focus() // 편집기에 커서 보여주기
+
+        }
+    })
+
+    postContents.addEventListener('blur', function(event){
+        // 편집기가 blur될 때 마지막 커서 위치에 있는 엘리먼트
+        lastCaretLine = document.getSelection().anchorNode
+        console.log(lastCaretLine.parentNode, lastCaretLine, lastCaretLine.length)
+    })
+
 })
+
+// 공백 엘리먼트 생성
+function createNewLine(){
+    const newline = document.createElement('div')
+    newline.innerHTML = `<br>`
+    return newline
+}
+
+function addFileToCurrentLine(line, file){
+    console.log(line.nodeType) // nodeType = 3 - 텍스트 노드
+    if(line.nodeType === 3){
+        line = line.parentNode
+        console.log(line)
+    }
+    line.insertAdjacentElement('afterend', createNewLine())
+    line.nextSibling.insertAdjacentElement('afterbegin', file)
+    line.nextSibling.insertAdjacentElement('afterend', createNewLine())
+    return line.nextSibling.nextSibling // 파일 하단에 위치한 공백라인
+}
