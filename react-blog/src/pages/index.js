4af4edb3fc6238b390e38123daf71b0cf509a71c
Galaxy <js_park95@naver.com>
2023-09-13 13:59:42 +0900

    리액트 페이지 컴포넌트 생성

---
+++ react-blog/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Root } from './Root'
+export { default as Home } from './Home'
+export { default as Story } from './Story'
+export { default as Post } from './Post'
+export { default as NotFound } from './NotFound'
---
+++ react-blog/src/pages/root.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Root(){
+    return (
+        <h1>Root</h1>
+    )
+}
+
+export default Root
